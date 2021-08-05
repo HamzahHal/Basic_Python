6888a50e1cadcc458e4adc6a4263c9f2fc9a5597
HamzahHal <hamzah.halim@hotmail.com>
2021-08-05 12:41:35 +0100

    commit

--- Fibonacci.py
+++ Fibonacci.py
@@ -21,7 +21,7 @@ a = int(input("Enter the terms: "))
 first = 0                                         # first element of series
 second = 1                                         # second element of series
 if a <= 0:
-    print("The requested series is", f)
+    print("The requested series is", first)
 else:
     print(first, second, end=" ")
     for x in range(2, a):
