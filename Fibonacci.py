2a220d25840f506be86bd1335c192b4575369835
HamzahHal <hamzah.halim@hotmail.com>
2021-08-05 12:40:52 +0100

    commit

--- Fibonacci.py
+++ Fibonacci.py
@@ -18,14 +18,14 @@ else:
 # Loop Fibonacci
 # Enter number of terms needed                   #0,1,1,2,3,5....
 a = int(input("Enter the terms: "))
-f = 0                                         # first element of series
-s = 1                                         # second element of series
+first = 0                                         # first element of series
+second = 1                                         # second element of series
 if a <= 0:
     print("The requested series is", f)
 else:
-    print(f, s, end=" ")
+    print(first, second, end=" ")
     for x in range(2, a):
-        next = f + s
-        print(next, end=" ")
-        f = s
-        s = next
+        Next = first + second
+        print(Next, end=" ")
+        first = second
+        second = Next
