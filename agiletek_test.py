56d0ed1792056f2e2fa86f622f783ad141c86ecf
HamzahHal <hamzah.halim@hotmail.com>
2021-08-07 13:11:47 +0100

    commit

--- agiletek_test.py
+++ agiletek_test.py
@@ -1,36 +1,44 @@
+import numpy as np
 import pandas as pd
-# importing the requests library
 import requests
+from requests.structures import CaseInsensitiveDict
+# import urllib library
+from urllib.request import urlopen
+# import json
+import json
 
 
 def read_data():
+    Dataframe = pd.read_csv("coding_exam.csv")
+    return Dataframe
+
+def api():
     dataset = pd.read_csv('coding_exam.csv')
-    return dataset
+    post_data = {}
+    post_data["Project Code"] = dataset.iloc[0,0]
+    return post_data
+
+
+try:
+    # store the URL in url as
+    # parameter for urlopen
+    url = "https://api.agiletek.co.uk/cps%60"
+
+    # store the response of URL
+    response = urlopen(url)
+
+    # storing the JSON response
+    # from url in data
+    data_json = json.loads(response.read())
+
+    # print the json response
+    print(data_json)
+except:
+    print("not working")
+
 
 
 print(read_data())
-
-
-def api():
-    try:
-
-        API_ENDPOINT = "https://api.agiletek.co.uk/cps%60"
-
-        API_KEY = "58791d45aa40fa2508bccd57"
-
-         # data to be sent to api
-        data = {'api_dev_key': API_KEY,
-            'api_option': 'paste',
-            'api_paste_format': 'python'}
-
-        # sending post request and saving response as response object
-        r = requests.post(url=API_ENDPOINT, data=data)
-
-        # extracting response text
-        pastebin_url = r.text
-        print("The pastebin URL is:%s" % pastebin_url)
-    except requests.exceptions:
-        print("ApI ISSUES")
-
-
 print(api())
+
+
