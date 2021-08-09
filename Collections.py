6dc8465c2d3ea46249de07864989795f72da268f
HamzahHal <hamzah.halim@hotmail.com>
2021-08-09 12:14:23 +0100

    commit

---
+++ Collections.py
@@ -0,0 +1,43 @@
+# collections: Counter, namedtupled, OrderedDict, defaultdict, deque
+from collections import Counter
+from collections import namedtuple
+from collections import OrderedDict
+from collections import defaultdict
+from collections import deque
+
+a = "aaaaabbbbccccc"
+my_counter = Counter(a)
+print(my_counter)
+print(my_counter.items())
+print(my_counter.values())
+print(my_counter.keys())
+print(my_counter.most_common(2))
+print(my_counter.most_common(1)[0][0])
+print(list(my_counter.elements()))
+
+
+Point = namedtuple('Point', 'x,y')
+pt = Point(45, 4)
+print(pt)
+print(pt.x)
+print(pt.y)
+
+
+ordered_dict = OrderedDict()
+ordered_dict['a'] = 1
+ordered_dict['b'] = 2
+ordered_dict['c'] = 3
+ordered_dict['d'] = 4
+print(ordered_dict)
+
+
+d = defaultdict(int)
+d['a'] = 1
+d['b'] = 2
+print(d['a'])
+print(d['b'])
+print(d['f'])
+
+
+
+
