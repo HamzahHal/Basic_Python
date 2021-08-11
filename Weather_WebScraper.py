c8b52729b41b1765188cef4a0d71242eb16c3eca
HamzahHal <hamzah.halim@hotmail.com>
2021-08-11 14:05:32 +0100

    commit

--- Weather_WebScraper.py
+++ Weather_WebScraper.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-from datetime import datetime
 
-url = 'https://forecast.weather.gov/MapClick.php?lat=' + 'latitude' + '&long=' + 'longitude'
-page = requests.get(url)
-print(page)
+
+html_text = requests.get('https://uk.indeed.com/jobs?q=python&l=United+Kingdom').text
+soup = BeautifulSoup(html_text, 'lxml')
+jobs = soup.findAll('td', class_="resultContent")
+#job_title = jobs.findAll('h2class_="jobTitle jobTitle-color-purple").text.replace(' ', '')
+#print(job_title)
+print(jobs)
